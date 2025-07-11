df2aa2a94e548be216755d39013924b329d30cc2
Muhammad Taimor Alam <alamtaimor@gmail.com>
2025-07-11 19:29:18 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Portfolio Website</h1>
+      <p>Coming soon...</p>
+    </div>
+  );
+}
+
+export default App;
