2c0e51d4958d1040422d617d8d5f4203213b9ae7
Taimor Alam <alamtaimor@gmail.com>
2025-07-21 16:22:02 +0100

    Updated skills

---
+++ skills.js
@@ -0,0 +1,51 @@
+export const skillsData = [
+  {
+    category: "Frontend",
+    skills: [
+      "JavaScript", 
+      "TypeScript", 
+      "React.js",
+      "SvelteKit", 
+      "Next.js",
+      "Mermaid.js", 
+      "Tailwind CSS", 
+      "HTML5",
+      "CSS3"
+    ]
+  },
+  {
+    category: "Backend",
+    skills: [
+      "Supabase",
+      "PostgreSQL",
+      "MYSQL",
+      "PHP",
+      "AWS S3,VPC",
+      "Node.js", 
+      "Express", 
+      "Python Flask", 
+      "GraphQL", 
+      "MongoDB"
+    ]
+  },
+  {
+    category: "DevOps & Tools",
+    skills: [
+      "Git, GitLab & GitHub", 
+      "Docker", 
+      "CI/CD Pipelines", 
+      "AWS", 
+    ]
+  },
+  {
+    category: "Soft Skills",
+    skills: [
+      "Problem Solving", 
+      "Communication", 
+      "Team Leadership", 
+      "Agile Methodology", 
+      "Mentoring", 
+      "Project Management"
+    ]
+  }
+];
